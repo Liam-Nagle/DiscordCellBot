57e82ddbb5ca3857902917da3aa46fc9bf06e82e
Liam Nagle <liamnagle5@gmail.com>
2019-08-04 00:55:12 +0100

    help-command-changes-embed-changes

--- bot.js
+++ bot.js
@@ -87,7 +87,6 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 			case 'help':
 			
 			var embeded = {
-			  "content": "this `supports` __a__ **subset** *of* ~~markdown~~ 😃 ```js\nfunction foo(bar) {\n  console.log(bar);\n}\n\nfoo(1);```",
 			  "embed": {
 				"title": "title ~~(did you know you can have markdown here too?)~~",
 				"description": "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
@@ -138,7 +137,53 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 			
                bot.sendMessage({
                    to: channelID,
-                   message: embeded
+                   message: 			  
+					"embed": {
+						"title": "title ~~(did you know you can have markdown here too?)~~",
+						"description": "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
+						"url": "https://discordapp.com",
+						"color": 14516329,
+						"timestamp": "2019-08-03T23:47:20.750Z",
+						"footer": {
+						  "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+						  "text": "footer text"
+						},
+						"thumbnail": {
+						  "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+						},
+						"image": {
+						  "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+						},
+						"author": {
+						  "name": "author name",
+						  "url": "https://discordapp.com",
+						  "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+						},
+						"fields": [
+						  {
+							"name": "🤔",
+							"value": "some of these properties have certain limits..."
+						  },
+						  {
+							"name": "😱",
+							"value": "try exceeding some of them!"
+						  },
+						  {
+							"name": "🙄",
+							"value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
+						  },
+						  {
+							"name": "<:thonkang:219069250692841473>",
+							"value": "these last two",
+							"inline": true
+						  },
+						  {
+							"name": "<:thonkang:219069250692841473>",
+							"value": "are inline fields",
+							"inline": true
+						  }
+						]
+					 }
 			   });
             break;
 			
