f9ed2ab1410a111966faf94081d416aea18b85fd
Liam Nagle <liamnagle5@gmail.com>
2019-08-03 02:43:18 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -92,4 +92,4 @@ bot.on('message', function (user, userID, channelID, message, evt) {
      }
 });
 
-client.login(process.env.NjA2OTk5MjkwODI0NjIyMDgw.XUTjUw.t88TJHE_6-E3Vq5l0qYfnphmRDM)
+client.login(process.env.BOT_TOKEN)
